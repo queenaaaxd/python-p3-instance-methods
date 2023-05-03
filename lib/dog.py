d52dc16fcca828d273cf0f90993ad61add9417f1
+ <+>
2023-05-03 15:05:30 -0400

    completed lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,22 @@
 #!/usr/bin/env python3
+import ipdb
 
 class Dog:
     # Class body goes here
 
     #Instance method definition
-    pass
+    def bark(self):
+        print("Woof!")
+        # rocky = Dog()
+        # rocky.bark()
+        # Woof!
+
+    # add instance method sit()
+    def sit(self):
+        print("The dog is sitting.")
+        # fido = Dog()
+        # fido.sit()
+        # The dog is sitting.
+
+# ipdb.set_trace()
+
--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
+import ipdb
 
 class Person:
     # Class body goes here
 
     #Instance method definition
-    pass
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
+
